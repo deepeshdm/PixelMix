29c94bb407b99b5fda7a59c31bdce546f9e23c08
unknown <deepeshmhatre133@gmail.com>
2022-03-02 17:08:52 +0530

    formatted some code

--- App.py
+++ App.py
@@ -7,7 +7,8 @@ from API import transfer_style
 
 
 # Set page configs. Get emoji names from WebFx
-st.set_page_config(page_title="PixelMix - Style Transfer", page_icon="./assets/favicon.png", layout="centered")
+st.set_page_config(page_title="PixelMix - Style Transfer",
+                   page_icon="./assets/favicon.png", layout="centered")
 
 # -------------Header Section------------------------------------------------
 
@@ -16,63 +17,57 @@ st.markdown(title, unsafe_allow_html=True)
 
 
 st.markdown(
-   "<b> <i> Create Digital Art using Machine Learning ! </i> </b>  &nbsp; We takes 2 images — Content Image & Style Image — and blends "
+    "<b> <i> Create Digital Art using Machine Learning ! </i> </b>  &nbsp; We takes 2 images — Content Image & Style Image — and blends "
     "them together so that the resulting output image retains the core elements of the content image, but appears to "
-    "be “painted” in the style of the style reference image.",unsafe_allow_html=True
-   )
+    "be “painted” in the style of the style reference image.", unsafe_allow_html=True
+)
 
 
 # Example Image
 st.image(image="./assets/nst.png")
-st.markdown("</br>",unsafe_allow_html=True)
+st.markdown("</br>", unsafe_allow_html=True)
 
 
 # -------------Sidebar Section------------------------------------------------
 
 
 with st.sidebar:
-    
+
     st.image(image="./assets/speed-brush.gif")
-    st.markdown("</br>",unsafe_allow_html=True)
+    st.markdown("</br>", unsafe_allow_html=True)
 
-    st.markdown('<p style="font-size: 25px;font-weight: 550;">Some Inspiration 🎨</p>', unsafe_allow_html=True)
-    st.markdown('Below are some of the art we created using PixelMix.', unsafe_allow_html=True)
+    st.markdown('<p style="font-size: 25px;font-weight: 550;">Some Inspiration 🎨</p>',
+                unsafe_allow_html=True)
+    st.markdown('Below are some of the art we created using PixelMix.',
+                unsafe_allow_html=True)
 
+    # ---------------------Example art images------------------------------
 
-    #---------------------Example art images------------------------------
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(image="./assets/content1.jpg")
+    with col2:
+        st.image(image="./assets/art1.png")
 
-    col1,col2 = st.columns(2)
-    with col1 : 
-            st.image(image="./assets/content1.jpg")
-    with col2 : 
-            st.image(image="./assets/art1.png")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(image="./assets/content2.jpg")
+    with col2:
+        st.image(image="./assets/art2.png")
 
-    
-    col1,col2 = st.columns(2)
-    with col1 : 
-            st.image(image="./assets/content2.jpg")
-    with col2 : 
-            st.image(image="./assets/art2.png")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(image="./assets/content3.jpg")
+    with col2:
+        st.image(image="./assets/art3.png")
 
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(image="./assets/content4.jpg")
+    with col2:
+        st.image(image="./assets/art4.png")
 
-    col1,col2 = st.columns(2)
-    with col1 : 
-            st.image(image="./assets/content3.jpg")
-    with col2 : 
-            st.image(image="./assets/art3.png")
-
-    
-    col1,col2 = st.columns(2)
-    with col1 : 
-            st.image(image="./assets/content4.jpg")
-    with col2 : 
-            st.image(image="./assets/art4.png")
-
-
-     #----------------------------------------------------------------------
-
-
-
+ # ----------------------------------------------------------------------
 
     # line break
     st.markdown(" ")
@@ -82,7 +77,6 @@ with st.sidebar:
                 "[*github.com/deepeshdm*](https://github.com/deepeshdm/Realtime_Face_Detection)")
 
 
-
 # -------------Body Section------------------------------------------------
 
 # Upload Images
@@ -90,58 +84,55 @@ col1, col2 = st.columns(2)
 content_image = None
 style_image = None
 with col1:
-        content_image = st.file_uploader("Upload Content Image (PNG & JPG images only)", type=['png', 'jpg'])
+    content_image = st.file_uploader(
+        "Upload Content Image (PNG & JPG images only)", type=['png', 'jpg'])
 with col2:
-        style_image = st.file_uploader("Upload Style Image (PNG & JPG images only)", type=['png', 'jpg'])
+    style_image = st.file_uploader(
+        "Upload Style Image (PNG & JPG images only)", type=['png', 'jpg'])
 
 
-st.markdown("</br>",unsafe_allow_html=True)
+st.markdown("</br>", unsafe_allow_html=True)
 st.warning('NOTE : You need atleast Intel i3 with 8GB memory for proper functioning of this application.')
 
 
-
 if content_image is not None and style_image is not None:
-    
-        with st.spinner("Styling Images...will take about 20-30 secs"):
 
-            content_image = Image.open(content_image)
-            style_image = Image.open(style_image)
+    with st.spinner("Styling Images...will take about 20-30 secs"):
 
-            # Convert PIL Image to numpy array
-            content_image = np.array(content_image)
-            style_image = np.array(style_image)
+        content_image = Image.open(content_image)
+        style_image = Image.open(style_image)
 
-            # Path of the pre-trained TF model 
-            model_path = r"model"
+        # Convert PIL Image to numpy array
+        content_image = np.array(content_image)
+        style_image = np.array(style_image)
 
-            # output image
-            styled_image = transfer_style(content_image,style_image,model_path)
-            if style_image is not None:
-                        # some baloons
-                        st.balloons()
+        # Path of the pre-trained TF model
+        model_path = r"model"
 
-            col1,col2 = st.columns(2)
-            with col1:
-                # Display the output
-                st.image(styled_image)
-            with col2:
-                
-                st.markdown("</br>",unsafe_allow_html=True)
-                st.markdown("<b> Your Image is Ready ! Click below to download it. </b>",unsafe_allow_html=True)
+        # output image
+        styled_image = transfer_style(content_image, style_image, model_path)
+        if style_image is not None:
+            # some baloons
+            st.balloons()
 
+        col1, col2 = st.columns(2)
+        with col1:
+            # Display the output
+            st.image(styled_image)
+        with col2:
 
-                # de-normalize the image
-                styled_image = (styled_image * 255).astype(np.uint8)
-                # convert to pillow image
-                img = Image.fromarray(styled_image)
-                buffered = BytesIO()
-                img.save(buffered, format="JPEG")
-                st.download_button(
-                            label="Download image",
-                            data=buffered.getvalue(),
-                            file_name="output.png",
-                            mime="image/png")
+            st.markdown("</br>", unsafe_allow_html=True)
+            st.markdown(
+                "<b> Your Image is Ready ! Click below to download it. </b>", unsafe_allow_html=True)
 
-
-
-
+            # de-normalize the image
+            styled_image = (styled_image * 255).astype(np.uint8)
+            # convert to pillow image
+            img = Image.fromarray(styled_image)
+            buffered = BytesIO()
+            img.save(buffered, format="JPEG")
+            st.download_button(
+                label="Download image",
+                data=buffered.getvalue(),
+                file_name="output.png",
+                mime="image/png")
